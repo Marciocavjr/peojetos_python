2a35426299353fc2e6d9351e9b46857b85825c31
Marciocavjr <marcio.jrc@hotmail.com>
2025-11-25 16:03:29 -0300

    Código finalizado e realizada todas as alterações.

--- jogo.py
+++ jogo.py
@@ -2,7 +2,7 @@ import random
 
 def iniciar_jogo():
     print("bem vindo ao jogo de adivinhação!")
-    numero_secreto = random.randint(1, 100)
+numero_secreto = random.randint(1, 100)
 
 tentativas = 0
 
@@ -10,11 +10,13 @@ while True:
     entrada = input("digite seu número: ")
     chute = int(entrada)
     tentativas = tentativas + 1
+
     if chute == numero_secreto:
         print(f"Parabéns! Você acertou em {tentativas} tentativas!")
         break
     elif chute > numero_secreto:
         print("Seu palpite foi alto. Tente um número menor.")
     else:
-        print("Seu palpite foi baixo. Tente um número menor.")
+        print("Seu palpite foi baixo. Tente um número maior.")
 
+iniciar_jogo()
